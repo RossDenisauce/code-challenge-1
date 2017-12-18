3ff62cbe9ae78ba063e9f10f7b0803f1e5eebfa3
Ross <ross@tests-MacBook-Pro.local>
2017-12-18 13:39:29 -0600

    part 6

--- client.js
+++ client.js
@@ -5,14 +5,19 @@ var counter = 0;
 function start(){
   console.log('In DOM');
   $('#generateBtn').on('click', addDiv);
+  $('body').on('click', '#swapBtn',swapButton);
 }
 
 function addDiv(){
   counter++;
   var newDiv = $('<div></div>');
   newDiv.append('<p>' + counter + '</p>');
-  newDiv.append('<button class="newBtns">Swap</button>');
-  newDiv.append('<button class="newBtns">Delete</button>');
-
+  newDiv.append('<button id="swapBtn">Swap</button>');
+  newDiv.append('<button id="deletebtn">Delete</button>');
   $('body').append(newDiv);
+}
+
+function swapButton(){
+  //$(this).closest('div').css('background-color', 'yellow');
+  $(this).closest('div').toggleClass('yellow');
 }
